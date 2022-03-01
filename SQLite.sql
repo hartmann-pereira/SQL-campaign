e09ab53470af1119802f6245153fe812ab6afc30
hartmann.pereira <hartmann.pereira@gmail.com>
2022-03-01 01:15:10 +0000

    Final update to the sql file
    Added final queries

--- SQLite.sql
+++ SQLite.sql
@@ -1,8 +1,18 @@
+--This SQL file contains queries about a walmart retail dataset between 2012 and 2015--
+--Built importing a csv file into sqloteonline.com and querying the data--
+--Created by Ricardo Hartmann--
+--Available on github--
+
+
 -- State wise average profits--
 select state, avg(profit) as average_profit
 from walmartRetailData
 group by state
 order by average_profit desc;
+
+--Showing between what dates is our walmart dataset
+select min(order_date), max(order_date)
+from walmartRetailData;
 
 -- In wich age the company is getting the maximum profit--
 SELECT DISTINCT(customer_age), avg(profit) as average_profit
@@ -54,12 +64,11 @@ where customer_segment like 'Corporate';
 --Using a subquery to show what % of profit are coming from consumer clients
 select customer_segment, sum(profit)/
 	(select sum(walm.profit)
-     from walmartRetailData as walm
-     )*100 as Percentage_of_profit_coming_from_corporate_clients
+     from walmartRetailData as walm)*100 as Percentage_of_profit_coming_from_corporate_clients
 from walmartRetailData
 where customer_segment like 'Consumer';
 
-
+--Showing the number of products bought with a discount of 0.1--
 select DISTINCT(discount),
 	(select count(discount)
      from walmartRetailData
@@ -73,12 +82,20 @@ from walmartRetailData
 where product_name like 'Xerox %'
 order by discount;
 
---Showing the min and max discount for each product category, we can see that only furniture has 0.25% discount
+--Showing the min and max discount for each product category, we can see that only furniture has products with 0.25% discount
 select max(discount), min(discount), product_category
 from walmartRetailData
 GROUP by product_category;
 
 
+--Showing the profits and number of orders for each type of order (high, low, medium, critical and null) and profits per order
+select DISTINCT(order_priority) as type_of_order,
+	sum(profit) as profits_per_type_of_order,
+	sum(number_of_records) as number_of_orders,
+    sum(profit)/sum(number_of_records) as profits_per_order
+from walmartRetailData
+GROUP by type_of_order
+order by sum(profit) DESC;
 
 
 
